f0270b7f3d6c71a360d159975c9d3ad2004a6357
John <jfair@byui.edu>
2018-09-23 19:08:55 -0600

    Prove 1 commit

---
+++ cs450/Prove1.py
@@ -0,0 +1,22 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+iris = datasets.load_iris()
+
+class HardCodedModel:
+    def predict(self, test):
+        return [0] * len(test)
+
+class HardCodedClassifier:
+    def fit(self, d_train, t_target):
+        return HardCodedModel()
+
+d_train, d_test, t_train, t_test = train_test_split(iris.data, iris.target, test_size =0.3, random_state=7)
+
+#comment out either classifier
+classifier = HardCodedClassifier()
+#classifier = GaussianNB()
+
+model = classifier.fit(d_train, t_train)
+targets_predicted = model.predict(d_test)
