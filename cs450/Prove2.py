c9264353212b2488ebd7aa9a4cda0226c092c4d4
John <jfair@byui.edu>
2018-09-29 23:42:00 -0600

    Prove 2 commit -- updated 9/29/18

---
+++ cs450/Prove2.py
@@ -0,0 +1,26 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+iris = datasets.load_iris()
+
+class kNNCodedModel:
+    def __init__(self, iris_data):
+        self.iris_data = iris
+
+    def predictkNN(self):
+        predict = []
+        predict.append(0)
+        return predict
+
+class kNNCodedClassifier:
+    def fit(self, d_train, t_target):
+        return kNNCodedModel
+
+d_train, d_test, t_train, t_test = train_test_split(iris.data, iris.target, test_size =0.3, random_state=7)
+
+classifier = KNeighborsClassifier(n_neighbors=3)
+model = classifier.fit(d_train, t_train)
+predictions = model.predict(d_train)
+
+print(d_test)
